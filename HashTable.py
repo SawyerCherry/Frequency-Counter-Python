83c25f7148b3460bae5b5faaf296227f41c87056
Sawyer Cherry <sawyeracherry@gmail.com>
2021-02-22 17:15:03 -0600

    Completed create_arr function

--- HashTable.py
+++ HashTable.py
@@ -13,10 +13,19 @@ class HashTable:
   # This method creates an array (list) of a given size and populates each of its elements with a LinkedList object.
 
   def create_arr(self, size):
-    pass
+    # set counter to start at 0
+    counter = 0 
+    # create an empty for the array
+    arr = []
 
-
-
+    while counter < size:
+      #append the linked list to the array
+      thing = LinkedList()
+      #appending thing to the list as many times as we need
+      arr.append(thing)
+      counter += 1
+    
+    return arr
 
   # 2️⃣ TODO: Create your own hash function.
 
@@ -28,12 +37,18 @@ class HashTable:
 
 
 
+
+
   # 3️⃣ TODO: Complete the insert method.
 
   # Should insert a key value pair into the hash table, where the key is the word and the value is a counter for the number of times the word appeared. When inserting a new word in the hash table, be sure to check if there is a Node with the same key in the table already.
 
   def insert(self, key, value):
     pass
+
+
+
+    
 
 
 
